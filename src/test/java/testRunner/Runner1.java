016d0be2de73c02a41e689b20f39cbab4411ee09
Signity <Signity@DESKTOP-767N599>
2024-07-04 16:46:39 +0530

    check latest comment changes

--- src/test/java/testRunner/Runner1.java
+++ src/test/java/testRunner/Runner1.java
@@ -7,5 +7,5 @@ import io.cucumber.testng.CucumberOptions;
 "C:\\Users\\Signity\\eclipse-workspace\\Selenium_MrngBatch\\Cucumber_Framework\\FeatureFile" }, monochrome = true, tags = "@tag2", glue = "stepDefination", plugin = {"pretty", "html:test-output/cucumber/report2.html", "json:test-output/cucjson-reports/Cucumber.json" } )
 public class Runner1 extends AbstractTestNGCucumberTests {
 	//extendrcheck for scenario with simple changes
-	//again comment check
+	//again comment check basic things
 }
