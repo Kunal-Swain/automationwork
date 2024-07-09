31b81cffacc65920f2da2954fb8e0fbb89a15160
Signity <Signity@DESKTOP-767N599>
2024-07-09 17:26:32 +0530

    commiting

--- src/test/java/testRunner/Runner1.java
+++ src/test/java/testRunner/Runner1.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {
-"C:\\Users\\Signity\\eclipse-workspace\\Selenium_MrngBatch\\Cucumber_Framework\\FeatureFile" }, monochrome = true, tags = "@tag3", glue = "stepDefination", plugin = {"pretty", "html:test-output/cucumber/report2.html", "json:test-output/cucjson-reports/Cucumber.json" } )
+"C:\\Users\\Signity\\eclipse-workspace\\Selenium_MrngBatch\\Cucumber_Framework\\FeatureFile" }, monochrome = true, tags = "@tag2", glue = "stepDefination", plugin = {"pretty", "html:test-output/cucumber/report2.html", "json:test-output/cucjson-reports/Cucumber.json" } )
 public class Runner1 extends AbstractTestNGCucumberTests {
 	//extendrcheck for scenario with simple changes
 	//again comment check basic things
